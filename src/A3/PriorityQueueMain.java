2b9e58016c665122dcd6a5c4aaa76961311ca2f7
narrapangan <narra_pangan@hotmail.com>
2019-03-13 13:10:27 -0400

    Added temporary test cases.

--- src/A3/PriorityQueueMain.java
+++ src/A3/PriorityQueueMain.java
@@ -12,12 +12,15 @@ public class PriorityQueueMain {
 		System.out.println("\nPriority Queue created");			
 		System.out.println("\nAbout to insert 1 entry with random keys into the Priority Queue!");
 //			// inserting random keys with element
+//		pq.remove(); // should stop
 		pq.insert("person 1", randomNum());
+		pq.remove(); // should remove person 1
 		pq.insert("person 2", randomNum());
 		pq.insert("person 3", randomNum());
 		pq.insert("person 4", randomNum());
 		pq.insert("person 5", randomNum());
 		pq.insert("person 6", randomNum());
+//		pq.insert("person 7", randomNum()); // over capacity
 
 	    System.out.println("\nThe PQ is now : " + pq.text());
 	    pq.remove();
